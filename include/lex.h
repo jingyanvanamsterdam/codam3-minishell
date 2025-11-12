29a0d545962b834e5be903a816a3fa798b881512
jingyan <jingyan.dong420@gmail.com>
2025-11-12 18:49:02 +0100

    Make some change at quote handling.

--- include/lex.h
+++ include/lex.h
@@ -1,8 +1,8 @@
 #ifndef LEX_H
 # define LEX_H
 
-#include "struct.h"
-#include <stdbool.h> // for boolean
+# include "struct.h"
+# include <stdlib.h>
 
 void	free_token_list(t_token **lst);
 void	free_env_lst(t_env **lst);
@@ -10,6 +10,7 @@ void	free_shell(t_shell *shell);
 void	free_split(char **key_value);
 
 void	ft_malloc_failure(char *s, t_shell *shell);
+void	ft_input_error(char	*s, t_shell *shell);
 
 t_env 	*init_env(char **envp, t_shell *shell);
 
@@ -18,10 +19,13 @@ char	*append_to_str(char *dst, char *src);
 t_token *tokenization(char *input, t_shell *shell);
 
 //Expands handling funcs. 
-size_t	contain_expands(char *str, size_t end);
-int		skip_expansion(char *str);
+size_t	expands_index(char *str, size_t end);
+size_t	skip_expansion(char *str);
 char	*lookup_key(t_env *env_lst, const char *key);
 char	*expansion(char *str, t_shell *shell, size_t end);
-char	*handle_expands(char *str, size_t len, t_shell *shell);
+char	*handle_expands(char *str, size_t i, size_t len, t_shell *shell);
+
+//Quote handling funcs.
+size_t	find_close_quote(char *str, size_t end, t_shell *shell);
 
 #endif
