c89b2a56de98daceaeed03698663ed08d32bc944
RayY721 <74128049+RayY721@users.noreply.github.com>
2025-11-15 16:33:54 +0100

    builtins function

---
+++ builtins/ft_env.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+#include "minishell.h"
+#include <unistd.h>
+
+// TODO: 这个函数和env里面的打印整个env list的一样呢，到时候可以把env里那个函数给替换掉
+int	ft_env(char **argv, t_env *env)
+{
+	(void)argv;
+	while (env)
+	{
+		if (env->value)
+		{
+			ft_putstr_fd(env->key, 1);
+			ft_putstr_fd("=", 1);
+			ft_putstr_fd(env->value, 1);
+			ft_putstr_fd("\n", 1);
+		}
+		env = env->next;
+	}
+	return (0);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: kuyu <kuyu@student.codam.nl>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 16:15:55 by ray               #+#    #+#             */
-/*   Updated: 2026/01/12 06:29:21 by kuyu             ###   ########.fr       */
+/*   Updated: 2026/01/12 20:02:05 by kuyu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,24 @@ int		ft_isspace(int c);
 char	*ft_itoa(int n);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_putstr_fd(char *s, int fd);
+double	ft_atof(char *str);
+int		ft_atoi(const char *nptr);
+void	*ft_calloc(size_t nmemb, size_t size);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isspace(int c);
+char	*ft_itoa(int n);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_putstr_fd(char *s, int fd);
 char	**ft_split(char const *s, char c);
+int		ft_strcheck(char *s, int (*f)(int));
+char	*ft_strchr(const char *s, int c);
+int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 int		ft_strcheck(char *s, int (*f)(int));
 char	*ft_strchr(const char *s, int c);
 int		ft_strcmp(const char *s1, const char *s2);
@@ -36,6 +53,5 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, int n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-
 
 #endif
