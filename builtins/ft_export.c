4f011afe60c45fc226d1b9562a3c86038b113bc6
RayY721 <74128049+RayY721@users.noreply.github.com>
2025-11-17 07:59:53 +0100

    working on ft_export

--- builtins/ft_export.c
+++ builtins/ft_export.c
@@ -4,10 +4,61 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static void	add_or_update_env()
+static	t_env *
+// TODO: 也许用JD做的那些个libft的添加env node的函数？？？
+
+static void	add_or_update_env(t_env **env, char *key, char *value)
 {
+	t_env	*cur;
+
+	cur = *env;
+	while (cur)
+	{
+		if (!ft_strcmp(cur->key, key))
+		{
+			if (value)
+			{
+				free(cur->value);
+				cur->value = ft_strdup(value);
+			}
+			return ;
+		}
+		cur = cur->next;
+	}
+	new = malloc(sizeof(t_env));
 
 }
+
+static void	add_or_update_env(t_env **env, char *key, char *value)
+{
+	t_env	*cur;
+	t_env	*new;
+
+	cur = *env;
+	while (cur)
+	{
+		if (!ft_strcmp(cur->key, key))
+		{
+			if (value)
+			{
+				free(cur->value);
+				cur->value = ft_strdup(value);
+			}
+			return ;
+		}
+		cur = cur->next;
+	}
+	new = malloc(sizeof(t_env));
+	if (!new)
+		return ;
+	new->key = ft_strdup(key);
+	new->value = NULL;
+	if (value)
+		new->value = ft_strdup(value);
+	new->next = *env;
+	*env = new;
+}
+
 
 static void	ft_export_env_list(t_env *env)
 {
@@ -31,18 +82,18 @@ int	ft_export(char **argv, t_env *env)
 	(void)env;
 	i = 1;
 	if (!argv[i])
-		return (ft_export_env_list(env), 0);			// TODO: replace the following section with a function
+		return (ft_export_env_list(env), 0);
 	while (argv[i])
 	{
 		sep = ft_strchr(argv[i], '=');
 		if (sep)
 		{
-			// add new env variableq
 			*sep = '\0';
-
+			add_or_update_env();
 			*sep = '=';
 		}
 		else
+			add_or_update_env();
 		++i;
 	}
 	return (0);
