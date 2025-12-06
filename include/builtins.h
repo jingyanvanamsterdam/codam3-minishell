88401ba61ca1d4fd6e0f67e5f6c7c52268679278
RayY721 <74128049+RayY721@users.noreply.github.com>
2025-12-06 13:27:31 +0100

    builtins function doen, now need to be tested.

--- include/builtins.h
+++ include/builtins.h
@@ -3,8 +3,12 @@
 
 # include "minishell.h"
 
-int	ft_pwd(char **argv, t_env *env);
-int	ft_echo(char **argv, t_env *env);
-int	ft_env(char **argv, t_env *env);
+int	ft_pwd(char **argv, t_shell *shell);
+int	ft_echo(char **argv, t_shell *shell);
+int	ft_env(char **argv, t_shell *shell);
+
+int	ft_unset(char **argv, t_shell *shell);
+int	ft_exit(char **argv, t_shell *shell);
+int	ft_export(char **argv, t_shell *shell);
 
 #endif
